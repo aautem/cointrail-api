773eafbbef6d62b557cf3245504a7c7a81d3c24a
Alex Autem <aautem@trifinlabs.com>
2017-09-24 20:17:01 -0700

    Update port number

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ app.get('/', function(req, res) {
   res.send('.~::  C O N T R A I L  ::~.');
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log('Listening on port 3000');
 });
