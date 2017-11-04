b368ed19ea3863d143695218030593e8fe6c0088
Alex Autem <autem.alex@gmail.com>
2017-11-04 16:02:51 -0700

    Set up socket server

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-const express = require('express');
-const app = express();
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 app.get('/', function(req, res) {
   res.send('.~::  C O N T R A I L  ::~.');
 });
 
-app.listen(process.env.PORT || 3000, function() {
+io.on('connection', function(socket) {
+  console.log('New Player:', socket);
+});
+
+http.listen(3000, function() {
   console.log('Listening on port 3000');
 });
