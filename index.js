dfc9710387fa6aa5a33638bb7ece40a8d4dfad30
Alex Autem <autem.alex@gmail.com>
2017-11-04 16:09:52 -0700

    Update port

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
 
 app.get('/', function(req, res) {
   res.send('.~::  C O N T R A I L  ::~.');
 });
 
-io.on('connection', function(socket) {
+io.on('connection', function(socket){
   console.log('New Player:', socket);
 });
 
-http.listen(3000, function() {
-  console.log('Listening on port 3000');
+http.listen(port, function() {
+  console.log(`Listening on port ${port}...`);
 });
