487c9b78d2151369cfabbb64129c9e48e9798e40
Alex Autem <autem.alex@gmail.com>
2017-11-05 03:15:11 -0800

    Comment out future lines

--- routes.js
+++ routes.js
@@ -1,10 +1,10 @@
 const statsCtrl = require('./stats/statsCtrl');
-const settingsCtrl = require('./settings/settingsCtrl');
-const leadersCtrl = require('./leaders/leadersCtrl');
-const msgCtrl = require('./msg/msgCtrl');
-const friendsCtrl = require('./friends/friendsCtrl');
-const historyCtrl = require('./history/historyCtrl');
-const gameCtrl = require('./game/gameCtrl');
+// const settingsCtrl = require('./settings/settingsCtrl');
+// const leadersCtrl = require('./leaders/leadersCtrl');
+// const msgCtrl = require('./msg/msgCtrl');
+// const friendsCtrl = require('./friends/friendsCtrl');
+// const historyCtrl = require('./history/historyCtrl');
+// const gameCtrl = require('./game/gameCtrl');
 
 module.exports = (app, express) => {
   app.get('/api/stats/:username', statsCtrl.loadStats);
