f571e3667d20689afddc2d5dbb8ce32ea6869537
Alex Autem <autem.alex@gmail.com>
2017-11-13 18:26:22 -0800

    Update messages

--- messages/messagesCtrl.js
+++ messages/messagesCtrl.js
@@ -32,18 +32,18 @@ module.exports = {
       } else if (!data) {
         console.log('\x1b[34m', 'No user found.');
       } else {
-        console.log('\x1b[34m', 'Sending message to ', data.username);
+        console.log('\x1b[34m', 'Sending message to', data.username);
 
-        const messages = new Messages({
-          username: req.params.username,
-          messages: data.messages.slice().unshift(req.body.message),
-        });
+        const messages = data.messages.slice();
+        messages.unshift(req.body.message);
 
-        messages.save((err, data) => {
+        Messages.findOneAndUpdate({ username: req.params.username }, messages, { new: true }, (err, data) => {
           if (err) {
-            console.log('\x1b[34m', 'Error saving messages:', err);
+            console.log('\x1b[34m', 'Error updating messages:', err);
+          } else if (!data) {
+            console.log('\x1b[34m', 'No messages found');
           } else {
-            console.log('\x1b[34m', 'Messages saved:', data.username);
+            console.log('\x1b[34m', 'Messages updated for', data.username);
             res.end(JSON.stringify('OK'));
           }
         });
