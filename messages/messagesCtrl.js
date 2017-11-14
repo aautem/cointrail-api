900ec4cc5f4c76071ba16264747e3773cf506f18
Alex Autem <autem.alex@gmail.com>
2017-11-13 18:30:23 -0800

    Add logs

--- messages/messagesCtrl.js
+++ messages/messagesCtrl.js
@@ -33,9 +33,12 @@ module.exports = {
         console.log('\x1b[34m', 'No user found.');
       } else {
         console.log('\x1b[34m', 'Sending message to', data.username);
+        console.log('\x1b[34m', 'Message:', req.body.message);
 
         const messages = data.messages.slice();
         messages.unshift(req.body.message);
+
+        console.log('\x1b[34m', 'New messages:', messages);
 
         Messages.findOneAndUpdate({ username: req.params.username }, messages, { new: true }, (err, data) => {
           if (err) {
@@ -44,6 +47,8 @@ module.exports = {
             console.log('\x1b[34m', 'No messages found');
           } else {
             console.log('\x1b[34m', 'Messages updated for', data.username);
+            console.log('\x1b[34m', 'Updated messages:', data);
+
             res.end(JSON.stringify('OK'));
           }
         });
