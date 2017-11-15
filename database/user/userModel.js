02f4faa1575d2f83f1988dfc3783e59fa0168121
Alex Autem <autem.alex@gmail.com>
2017-11-15 15:13:57 -0800

    Update user model

--- database/user/userModel.js
+++ database/user/userModel.js
@@ -1,6 +1,72 @@
 const mongoose = require('mongoose');
 const helpers = require('../../utilities/helpers');
 
+const SettingsSchema = new mongoose.Schema({
+  boardSize: {
+    type: Number,
+    default: 4,
+    required: true
+  },
+  timeLimit: {
+    type: Boolean,
+    default: false,
+    required: true
+  },
+  color: {
+    type: String,
+    default: helpers.getColor(),
+    required: true
+  },
+  altColor: {
+    type: String,
+    default: helpers.getAltColor(),
+    required: true
+  }
+});
+
+const StatsSchema = new mongoose.Schema({
+  wins: {
+    type: Number,
+    default: 0,
+    required: true
+  },
+  losses: {
+    type: Number,
+    default: 0,
+    required: true
+  },
+  draws: {
+    type: Number,
+    default: 0,
+    required: true
+  },
+  totalPoints: {
+    type: Number,
+    default: 0,
+    required: true
+  },
+  gamesPlayed: {
+    type: Number,
+    default: 0,
+    required: true
+  },
+  winsByDefault: {
+    type: Number,
+    default: 0,
+    required: true
+  },
+  winsByPoints: {
+    type: Number,
+    default: 0,
+    required: true
+  },
+  winsByConnect: {
+    type: Number,
+    default: 0,
+    required: true
+  }
+});
+
 const UserSchema = new mongoose.Schema({
   username: {
     type: String,
@@ -23,7 +89,7 @@ const UserSchema = new mongoose.Schema({
   inGame: {
     type: Boolean,
     default: false,
-    require: true
+    required: true
   },
   online: {
     type: Boolean,
@@ -31,68 +97,14 @@ const UserSchema = new mongoose.Schema({
     required: true
   },
   settings: {
-    boardSize: {
-      type: Number,
-      default: 4,
-      required: true
-    },
-    timeLimit: {
-      type: Boolean,
-      default: false,
-      required: true
-    },
-    color: {
-      type: String,
-      default: helpers.getColor(),
-      required: true
-    },
-    altColor: {
-      type: String,
-      default: helpers.getAltColor(),
-      required: true
-    }
+    type: SettingsSchema,
+    default: SettingsSchema,
+    required: true
   },
   stats: {
-    wins: {
-      type: Number,
-      default: 0,
-      required: true
-    },
-    losses: {
-      type: Number,
-      default: 0,
-      required: true
-    },
-    draws: {
-      type: Number,
-      default: 0,
-      required: true
-    },
-    totalPoints: {
-      type: Number,
-      default: 0,
-      required: true
-    },
-    gamesPlayed: {
-      type: Number,
-      default: 0,
-      required: true
-    },
-    winsByDefault: {
-      type: Number,
-      default: 0,
-      required: true
-    },
-    winsByPoints: {
-      type: Number,
-      default: 0,
-      required: true
-    },
-    winsByConnect: {
-      type: Number,
-      default: 0,
-      required: true
-    }
+    type: StatsSchema,
+    default: StatsSchema,
+    required: true
   },
   friends: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
 });
