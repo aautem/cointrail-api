9413960bd2131546adea584a7b722a9597665dbb
Alex Autem <autem.alex@gmail.com>
2017-11-15 16:33:49 -0800

    Message controller updates

--- database/message/messageCtrl.js
+++ database/message/messageCtrl.js
@@ -1,4 +1,5 @@
 const Message = require('./messageModel');
+const User = require('./userModel');
 const mongoose = require('mongoose');
 
 module.exports = {
@@ -15,23 +16,41 @@ module.exports = {
     });
   },
   createMessage: (req, res) => {
+
+    // to: <username>
+    // from: <sender id>
+    // type: 'message' | 'friend'
+    // message: String
+
     if (!req.body.to || !req.body.from || !req.body.message) {
       console.log('\x1b[31m', 'Data missing from message.');
       res.end(new Error('Data missing from message.'));
     } else {
-      const message = new Message({
-        to: mongoose.Types.ObjectId(req.body.to),
-        from: mongoose.Types.ObjectId(req.body.from),
-        type: req.body.type,
-        message: req.body.message,
-      });
-      message.save((err) => {
+      // make sure the user it's going to actually exists
+      User.findOne({ username: req.body.to }, (err, user) => {
         if (err) {
-          console.log('\x1b[31m', 'Error saving message:', err);
+          console.log('\x1b[31m', 'Error loading user:', err);
           res.end(JSON.stringify(err));
+        } else if (!user) {
+          console.log('\x1b[31m', 'User does not exist.');
+          res.end(new Error('User does not exist.'));
         } else {
-          console.log('\x1b[34m', 'Message saved:', message);
-          res.end(JSON.stringify(message));
+          // create message
+          const message = new Message({
+            to: mongoose.Types.ObjectId(user._id),
+            from: mongoose.Types.ObjectId(req.body.from),
+            type: req.body.type || 'message',
+            message: req.body.message,
+          });
+          message.save((err) => {
+            if (err) {
+              console.log('\x1b[31m', 'Error saving message:', err);
+              res.end(JSON.stringify(err));
+            } else {
+              console.log('\x1b[34m', 'Message saved:', message);
+              res.end(JSON.stringify(message));
+            }
+          });
         }
       });
     }
